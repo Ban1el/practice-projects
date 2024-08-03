1b14cefcc3e2b3131c7777e344a993f95b6b9d61
Ban1el <danieltroyaromin1337@gmail.com>
2024-08-03 20:08:52 +0800

    HRIS - Initial Commit

---
+++ HRIS/HRIS/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HRIS.Models.DTO;
+
+namespace HRIS.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            EmployeeDTO employeeDTO = new EmployeeDTO();
+
+            return View(employeeDTO);
+        }
+
+        [HttpPost]
+        public ActionResult Add(EmployeeDTO dto)
+        {
+            return Json("Success");
+        }
+    }
+}
---
+++ HRIS/HRIS/Models/DTO/CustomDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HRIS.Models.DTO
+{
+    public class EmployeeDTO
+    {
+        public int ID { get; set; }
+        public string EmployeeID { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string MobileNumber { get; set; }
+        public string Email { get; set; }
+        public DateTime? DateCreated { get; set; }
+        public DateTime? DateModified { get; set; }
+    }
+}
